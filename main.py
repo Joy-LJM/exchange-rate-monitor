df33fb37ad817bf7e99721eb528a989fd5c3020f
Joy-LJM <2522702541@qq.com>
2026-01-10 17:42:37 -0500

    monitor CAD exchange rate

---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+from datetime import datetime
+
+EMAIL = os.getenv("EMAIL_ADDRESS")
+APP_PSW = os.getenv("APP_PSW")
+SMTP_SERVER = os.getenv("SMTP_ADDRESS")
+
+URL = "https://www.cnyrate.com/cmb.html"
+HEADERS = {
+    "User-Agent": "Mozilla/5.0",
+    "Accept-Language": "en-US"
+}
+
+TARGET_RATE = 500.94
+
+response = requests.get(URL, headers=HEADERS, timeout=10)
+response.raise_for_status()
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+row = soup.find("tr", {"data-row-key": "CAD"})
+if not row:
+    raise ValueError("CAD row not found")
+
+tds = row.find_all("td")
+if len(tds) < 4:
+    raise ValueError("Not enough columns in CAD row")
+
+rate = round(float(tds[3].get_text(strip=True)), 2)
+print(f"[{datetime.now()}] CAD rate:", rate)
+
+if rate < TARGET_RATE:
+    subject = "CAD Exchange Rate Alert!"
+    body = f"CAD rate is now {rate}\n{URL}"
+
+    email_content = f"""From: {EMAIL}
+To: {EMAIL}
+Subject: {subject}
+
+{body}
+"""
+
+    with smtplib.SMTP(SMTP_SERVER, 587) as connection:
+        connection.starttls()
+        connection.login(EMAIL, APP_PSW)
+        connection.sendmail(
+            from_addr=EMAIL,
+            to_addrs=EMAIL,
+            msg=email_content.encode("utf-8")
+        )
+
